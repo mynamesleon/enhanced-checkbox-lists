5a09c3965c9c3c9c385c148be84c5c88e9517ccf
mynamesleon <contact@mynamesleon.com>
2020-02-05 19:16:00 +0000

    adjust export so that any existing enhanced list is destroyed if instantiated again

--- src/enhanced-checkbox-list.ts
+++ src/enhanced-checkbox-list.ts
@@ -1,13 +1,8 @@
 import EnhancedList from './enhanced-list';
 import EnhancedListApi from './enhanced-list-api';
 import EnhancedListOptions from './enhanced-list-options';
-import { API_STORAGE_PROP } from './enhanced-list-helpers';
 
 export function EnhancedCheckboxList(element: HTMLElement, options?: EnhancedListOptions): EnhancedListApi {
-    // if instance already exists on the main element, do not re-initialise
-    if (element && element[API_STORAGE_PROP] && element[API_STORAGE_PROP].open) {
-        return element[API_STORAGE_PROP];
-    }
     return new EnhancedList(element, options).api;
 }
 
